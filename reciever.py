851a42ea4b70650cea17d0d8946db7c4c6347cc5
hossainemruz <hossainemruz@gmail.com>
2021-10-24 19:47:34 +0600

    Send JSON message
    Signed-off-by: hossainemruz <hossainemruz@gmail.com>

--- reciever.py
+++ reciever.py
@@ -1,25 +1,49 @@
-import pika, sys, os
+import pika
+import json
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
+# Configuration Variables
 
-    channel.queue_declare(queue='hello')
+RABBIT_MQ_SERVER_URL="localhost"
+INPUT_QUEUE="test"
 
-    def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+# Create a global channel variable to hold our channel object in
+channel = None
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+# Step #2
+def on_connected(connection):
+    """Called when we are fully connected to RabbitMQ"""
+    # Open a channel
+    connection.channel(on_open_callback=on_channel_open)
 
+# Step #3
+def on_channel_open(new_channel):
+    """Called when our channel has opened"""
+    global channel
+    channel = new_channel
+    channel.queue_declare(queue=INPUT_QUEUE, durable=True, exclusive=False, auto_delete=False, callback=on_queue_declared)
+
+# Step #4
+def on_queue_declared(frame):
+    """Called when RabbitMQ has told us our Queue has been declared, frame is the response from RabbitMQ"""
     print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
+    channel.basic_consume(INPUT_QUEUE, handle_delivery,auto_ack=True)
 
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+# Step #5
+def handle_delivery(channel, method, header, body):
+    """Called when we receive a message from RabbitMQ"""
+    payload = json.loads(body)
+    print(payload["video"]["url"])
+    # json.dumps(payload)
+
+# Step #1: Connect to RabbitMQ using the default parameters
+parameters = pika.ConnectionParameters(host=RABBIT_MQ_SERVER_URL)
+connection = pika.SelectConnection(parameters, on_open_callback=on_connected)
+
+try:
+    # Loop so we can communicate with RabbitMQ
+    connection.ioloop.start()
+except KeyboardInterrupt:
+    # Gracefully close the connection
+    connection.close()
+    # Loop until we're fully closed, will stop on its own
+    connection.ioloop.start()
